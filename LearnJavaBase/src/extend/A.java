f65deefae97eea7fa3d5e84e7c3b39464d2ef9ce
emilyty <emilyty@163.com>
2017-07-25 16:39:16 +0800

    add:extend/A.java,extend/C.java

---
+++ LearnJavaBase/src/extend/A.java
@@ -0,0 +1,18 @@
+package extend;
+
+public class A {
+	private C c;
+	public A(C c){
+		this.c=c;
+	}
+	private void method2(){
+		System.out.println("method2");
+	}
+	public void method3(){
+		c.method3();
+	}
+	public void mehtodA(){
+		method2();
+		System.out.println("methodA");
+	}
+}
---
+++ LearnJavaBase/src/extend/C.java
@@ -0,0 +1,9 @@
+package extend;
+
+public class C {
+	private void method1(){
+		System.out.println("method1");}
+	public void method3(){
+		method1();
+		System.out.println("method3");}
+}
