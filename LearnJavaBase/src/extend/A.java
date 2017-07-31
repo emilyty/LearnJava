bd6f50c9621a8ec4c7889b5e6794811044603480
emilyty <emilyty@163.com>
2017-07-31 11:28:23 +0800

    update

--- LearnJavaBase/src/extend/A.java
+++ LearnJavaBase/src/extend/A.java
@@ -15,9 +15,4 @@ public class A {
 		method2();
 		System.out.println("methodA");
 	}
-	public void test(){
-		System.out.println("test master");
-	}
-	
-	public void newtes (){System.out.println("master");}
 }
